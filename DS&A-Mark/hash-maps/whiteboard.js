d9b798e36d730822bc1ecc92472dca032d9702fd
Vivian Thach <vivianlthach@gmail.com>
2022-05-03 12:01:43 -0700

    insertion - O(1), find - O(m) where m is worst case number of collisions

--- DS&A-Mark/hash-maps/whiteboard.js
+++ DS&A-Mark/hash-maps/whiteboard.js
@@ -34,29 +34,52 @@ console.log(arr) // => this item is placed at the 97th index of arr
 
 const hash = {};
 // hash collisions = 2 keys hashing to the same index
+
 // can fix this w/ linear probing - if it is colliding, assign it to the index over
+// O(n) linear time - worst case => linearArr is filled so we need to iterate to an index that isn't taken
+// where n = length from current index to next available spot
 const linearInsert = (key, val) => {
   const index = toHash(key);
   // arr[index] ? arr[index + 1] = val : arr[index] = val;
+  if (!linearArr[index]) return linearArr[index] = val;
   for(let i = index; i < linearArr.length + 1; i++) {
-    if (!linearArr[i]) return linearArr[i] = val;
+    if (!linearArr[i]) return linearArr[i] = val; // O(n)
   }
 
 }
 
 // use buckets!: instead of moving over, add it to the bucket at the index
+// O(1) insertion
 const insert = (key, val) => {
   const index = toHash(key);
   if (!newArr[index]) newArr[index] = [];
-  newArr[index].push(val);
+  newArr[index].push([key, val]);
 }
 
-linearInsert('arrows', 'yes');
+linearInsert('arrows', 'yes'); // O(1) - first time is constant but next = O(n)
 linearInsert('sworra', 'no');
 linearInsert('sarrow', 'maybe');
 console.log(linearArr);
 
+insert('bees', 'lots');
 insert('arrows', 'yes');
 insert('sworra', 'no');
 insert('sarrow', 'maybe');
 console.log(newArr);
+
+// collisions
+console.log(newArr[toHash('arrows')])
+
+// find a value given a key if there are collisions
+// O(m) time where m is the length of collisions at that hashed key index
+const find = key => {
+  const index = toHash(key);
+  if (newArr[index].length === 1) return newArr[index][1];
+  for(let item of newArr[index]) {
+    if (item[0] === key) return item[1];
+  }
+
+}
+
+console.log(find('arrows')) // O(m)
+console.log(find('bees')) // O(1)
